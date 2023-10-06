da8e02970631bd9c46335aaf1c0789003c7018c3
tct123 <42028373+tct123@users.noreply.github.com>
2023-10-06 04:16:31 +0200

    .

--- src/sysinfo/app.py
+++ src/sysinfo/app.py
@@ -18,7 +18,14 @@ class SysInfo(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
+        uname = platform.uname()
         main_box = toga.Box()
+        
+        # Widgets
+        system = toga.Label(text=f"System: {uname.system}")
+
+        # add
+        main_box.add(system)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
